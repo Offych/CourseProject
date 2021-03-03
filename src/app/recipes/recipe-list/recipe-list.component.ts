9632f0a3f921af6a84e33d540c1c484fb6dfe56c
Alexander Smotrikov <asmotrikov@fbsdata.com>
2021-03-03 18:22:08 +0300

    Recipe-lost component template markup

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'This is a test', 
+    "https://www.skinnytaste.com/wp-content/uploads/2009/02/turkey-meatloaf-8.jpg")
+  ];
 
   constructor() { }
 
